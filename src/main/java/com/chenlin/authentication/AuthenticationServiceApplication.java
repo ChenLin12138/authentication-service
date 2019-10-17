efd57c46fdc26de881c72ac38690fe4ee0431553
ChenLin12138 <scorpion_chenlin@163.com>
2019-10-17 22:20:01 +0800

    将authserver注册为eureka client

--- src/main/java/com/chenlin/authentication/AuthenticationServiceApplication.java
+++ src/main/java/com/chenlin/authentication/AuthenticationServiceApplication.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableResourceServer
+@EnableEurekaClient
 //告知spring cloud该服务作为OAuth2服务
 @EnableAuthorizationServer
 public class AuthenticationServiceApplication {
